4ab1c4456d221837d601cf62fdb317445ffc7c47
divine-mercyx <divinewilson766@gmail.com>
2025-09-03 12:10:27 +0100

    changed model

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const client = new Twilio(accountSid, authToken);
 const port = process.env.PORT || 3000;
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function getAIResponse(message) {
   const prompt = `
